06070e619891376d433f6c37abcd158c841be2e6
Mike578 <mikelbaz578@gmail.com>
2024-04-22 11:03:14 +0200

    update

---
+++ Generate API with Flask/Lesson/app1.py
@@ -0,0 +1,52 @@
+# Let's write your code here!
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello!"
+
+@app.route("/morning")
+def good_morning():
+    return "Good morning!"
+
+@app.route("/evening/<firstname>")
+def evening(firstname):
+    return f"Good evening, {firstname}!"
+
+@app.route("/greetings/<period_of_day>/<firstname>")
+def greetings(period_of_day, firstname):
+    return f"Good {period_of_day}, {firstname}!"
+
+@app.route("/add/<int:first>/<int:second>")
+def add(first, second):
+    return str(first + second)
+
+'''
+# Not working
+@app.route("/afternoon")
+def good_afternoon():
+    firstname = request.args['firstname']
+    return f"Good morning {firstname}!"
+
+# args['firstanme] renvoi un dct dont la clé est firstname mais ou la value n'existe pas d'où l'erreur
+'''
+
+# Run this instead:
+@app.route("/afternoon2")
+def good_afternoon2():
+    firstname = request.args.get('firstname', 'you')
+    return f"Good morning, {firstname}!"
+
+@app.route("/substract")
+def difference():
+    first = int(request.args.get('first', '0'))
+    second = int(request.args.get('second', '0'))
+    return str(first - second)
+
+
+@app.route("/hello")
+def hello_api():
+    return {"message": "Hello!", "hey": "I'm an API!"}
